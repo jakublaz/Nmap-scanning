2311a1aa4f1c4df466301fe7f3f81ab093c8eb72
jlaz <01178193@pw.edu.pl>
2025-12-16 11:50:02 +0100

    Presets optimized for faster and better scannig

--- scanner/presets.py
+++ scanner/presets.py
@@ -1,6 +1,6 @@
 PRESETS = {
     "fast": "-T4 -F",
-    "normal": "-sV",
-    "deep": "-A -p-",
-    "vuln": "--script vuln"
+    "normal": "-sV --version-intensity 5 -T4",
+    "deep": "-sS -sV -p- -T4 --min-rate 500 --max-retries 2",
+    "vuln": "-sV --script vuln --script-timeout 2m -T3"
 }
